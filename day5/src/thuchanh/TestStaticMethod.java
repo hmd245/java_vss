6c17b273b2394bc71ff52418d9ded60736c83d45
Hoang Minh Duc <hoangminhduc2451998@gmail.com>
2022-07-22 18:08:14 +0700

    add thuchanh5

---
+++ day5/src/thuchanh/TestStaticMethod.java
@@ -0,0 +1,18 @@
+package thuchanh;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+        // Gọi phương thức change
+        Student.change();
+
+        // Tạo đối tượng
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(112, "Khanh");
+        Student s3 = new Student(113, "Nam");
+
+        // Gọi phương thức display
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
