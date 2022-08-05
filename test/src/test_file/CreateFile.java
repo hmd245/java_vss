f70903d2227898ca1688555b38544ffac8d68078
duchm <hoangminhduc2451998@gmail.com>
2022-08-05 07:18:15 +0700

    thuchanh d9

--- test/src/test_file/CreateFile.java
+++ test/src/test_file/CreateFile.java
@@ -1,10 +1,11 @@
 package test_file;
 
 import java.io.File;  // Import the File class
+import java.io.FileWriter;
 import java.io.IOException;  // Import the IOException class to handle errors
 
 public class CreateFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             File myObj = new File("filename.txt");
             if (myObj.createNewFile()) {
@@ -16,5 +17,22 @@ public class CreateFile {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
+        String str = "File handing in Java using " +
+                "FileWriter and FileReader";
+
+        // attach a file to FileWriter
+        FileWriter fw = new FileWriter("output.txt");
+
+        // read character wise from string and write into FileWrite
+        for (int i = 0; i < str.length(); i++) {
+            fw.write(str.charAt(i));
+            //fw.write(5);
+            //fw.write('a');
+        }
+        System.out.println("Writing successful");
+
+        // close the file
+        fw.close();
     }
 }
