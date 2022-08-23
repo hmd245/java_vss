c1bf7122be4f3a2103a3a9bcea9f6d5e1f7ea36e
Hoang Minh Duc <hoangminhduc2451998@gmail.com>
2022-08-23 16:21:01 +0700

    add bt d20

---
+++ day20/demo-customer-management/src/main/java/com/codegym/democustomermanagement/DemoCustomerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.democustomermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCustomerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCustomerManagementApplication.class, args);
+	}
+
+}
---
+++ day20/demo-customer-management/src/main/java/com/codegym/democustomermanagement/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.codegym.democustomermanagement.service;
+
+import com.codegym.democustomermanagement.service.IGeneralService;
+import com.codegym.democustomermanagement.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
